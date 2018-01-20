b328aeac5ec5e63cb422c6e7da990db35c56377b
Tori Hall <vlhall95@gmail.com>
2018-01-20 14:54:22 -0500

    Create scrape.py

---
+++ scrape.py
@@ -0,0 +1,30 @@
+import praw
+import requests
+import requests.auth
+import pandas as pd
+import settings
+
+# authenticate
+reddit = praw.Reddit(client_id=settings.client_id,
+                     client_secret=settings.client_secret,
+                     user_agent=settings.user_agent)
+
+# test connection = true                
+print(reddit.read_only)
+
+# define start and end dates to scrape as Unix timestamps
+start = 1512086400 #1512086400 = beginning of december 
+end = 1512086460 #1514764800 = end of december
+
+# scrape all posts' subreddits and creation date
+subs = []
+dates = []
+for submission in reddit.subreddit('all').submissions(start, end):
+    subs.append(submission.subreddit)
+    dates.append(submission.created)
+
+# save to data frame 
+df = pd.DataFrame(list(zip(subs, dates)))
+df.columns = ['subreddit','date']
+#df.to_csv('reddit.csv')
+print df
