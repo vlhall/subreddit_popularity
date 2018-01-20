7524c2dd267e36d1190555130b237004da5b019a
Tori Hall <vlhall95@gmail.com>
2018-01-20 14:48:59 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
-# Visit http://praw.readthedocs.io/en/latest/getting_started/quick_start.html and follow the steps to authenticate via OAuth in order to follow along with the analysis
+# Visit http://praw.readthedocs.io/en/latest/getting_started/quick_start.html 
+# and follow the steps to authenticate via OAuth in order to follow along with the analysis
 
 client_id='INSERT_ID'
 client_secret='INSERT_SECRET'
