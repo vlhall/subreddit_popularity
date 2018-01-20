4d6f791aea30ba1c924d42878f0648aa2c7390af
Tori Hall <vlhall95@gmail.com>
2018-01-20 14:43:27 -0500

    Create settings.py
    Update authentication settings here

---
+++ settings.py
@@ -0,0 +1,5 @@
+# Visit http://praw.readthedocs.io/en/latest/getting_started/quick_start.html and follow the steps to authenticate via OAuth in order to follow along with the analysis
+
+client_id='INSERT_ID'
+client_secret='INSERT_SECRET'
+user_agent='INSERT_AGENT'
