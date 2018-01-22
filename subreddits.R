447c4b306b5b32d58c545d1132d6dc53fa89573c
Tori Hall <vlhall95@gmail.com>
2018-01-21 22:59:14 -0500

    Update subreddits.R

--- subreddits.R
+++ subreddits.R
@@ -20,25 +20,26 @@ library(dplyr)
 
 ## start session with desired number of cores, import data
 sc = sparkR.session("local[4]")
-# in this case, data is imported from a local file for reproducibility.
-# depending on the constraints of your own machine, you may wish to connect to
-# the data differently in order to access a greater volume
+
+# in this case, data is imported from a local file -
+# depending on the constraints of your own machine, you may wish to connect to the data differently 
 df <- read.df("reddit.csv", "csv",header = "true", inferSchema = "true", na.strings = "NA")
 
 ## manipulate data
+
 # create columns for month, day, hour, minute, and day of week
 res = select(df, df$subreddit, df$date) 
 res = mutate(res, month=month(res$date), day=dayofmonth(res$date), hour=hour(res$date), minute=minute(res$date))
 
-# EDIT: how do you wish to compare your data? (month, day, hour, minute)
+# EDIT: over what time periods do you want to compare your data? (OPTIONS: month, day, hour, minute)
 grp <- "hour"
 
-# EDIT: which subsets do you want to compare? (month = 1-12, day = 1-7, hour = 0-23, minute = 0-59)
+# EDIT: which periods do you want to compare? (OPTIONS: month = 1-12, day = 1-7, hour = 0-23, minute = 0-59)
 i <- 3
 j <- 4
 k <- 5
 
-# get counts by subreddit at level of the prior selection
+# get post counts by subreddit at level of the prior selection
 if (grp == "month"){
   res = group_by(res, res$month, res$subreddit) %>% count()
   col <- 2
@@ -64,13 +65,14 @@ res_i$rank_i = c(1:25)
 res_j$rank_j = c(1:25)
 res_k$rank_k = c(1:25)
 
-# transform to Spark dataframe 
+# transform back to Spark dataframe 
 i_s = as.DataFrame(res_i)
 j_s = as.DataFrame(res_j)
 k_s = as.DataFrame(res_k)
 
 ## calculate changes in rank
-# join first and second groups to calculate change in rank
+
+# join first and second groups, calculate change
 join = join(i_s, j_s, i_s$"subreddit" == j_s$"subreddit", "outer")
 colnames(join)[col] <- 'sub'
 change = select(join, join$sub, join$rank_i - join$rank_j)
@@ -80,7 +82,7 @@ w_change = join(j_s, change, j_s$"subreddit" == change$"sub", "outer")
 res_j = select(w_change, w_change$subreddit, w_change$count, w_change$`(rank_i - rank_j)`) %>%
   rename(., rank_change = .$`(rank_i - rank_j)`) %>% arrange(., desc(.$count)) %>% head(n=25)
 
-# join second and third groups to calculate change in rank
+# join second and third groups, calculate change
 join = join(j_s, k_s, j_s$"subreddit" == k_s$"subreddit", "outer")
 colnames(join)[col] <- 'sub'
 change = select(join, join$sub, join$rank_j - join$rank_k)
